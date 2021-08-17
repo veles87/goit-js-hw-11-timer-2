dbd04d3c751ce0e4083c199b4d18f9ea68380427
Maksim Chekhovskiy <veles.cafe@gmail.com>
2021-08-17 23:04:41 +0300

    2

--- timer.js
+++ timer.js
@@ -8,7 +8,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Jul 13, 2022 00:00:00",
+  let birthday = "Oct 30, 2021 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
